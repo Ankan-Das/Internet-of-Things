7f5393812a1cb8ee025404f039a4a10a63a91c21
Ankan-Das <46451509+Ankan-Das@users.noreply.github.com>
2019-05-26 01:58:19 +0530

    Update 15May_potentiometer_may15b.ino

--- 15May_potentiometer_may15b/15May_potentiometer_may15b.ino
+++ 15May_potentiometer_may15b/15May_potentiometer_may15b.ino
@@ -1,6 +1,6 @@
 #include <Servo.h>
 
-Servo S1;
+Servo SERVO1;
 
 int value = 0;
 int potenPIN = A2;
@@ -8,7 +8,7 @@ int Angle = 0;
 
 void setup() {
  Serial.begin(9600);
- S1.attach(9);
+ SERVO1.attach(9);
 }
 
 void loop() {
@@ -16,9 +16,9 @@ void loop() {
   value = analogRead(potenPIN);
 
   Serial.println(value);
-value=map(value,0,1023,0,179);
+  value = map(value,0,1023,0,179);
   Angle = (value);
   
-    S1.write(Angle);
+    SERVO1.write(Angle);
     delay(50);
 }
